e2a4959466d5bea04e723a5312796b7616a5a346
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-10-27 14:55:38 +0530

    Deployment changes done 6

--- app/auth/register/step2/page.tsx
+++ app/auth/register/step2/page.tsx
@@ -1,14 +1,10 @@
-// app/auth/register/step2/page.tsx
-"use client"; // This directive allows the use of client components
+"use client"; // Ensure this is present
 
 import React from 'react';
 import Header from "../../../../components/Header";
-import { useRouter } from "next/navigation";
 import Login from '@/components/Login';
 
 const RegisterStep2Page = () => {
-  const router = useRouter(); // Keep or remove based on usage
-
   return (
     <div>
       <Header />
