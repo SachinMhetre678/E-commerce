d2de4928c8ee7dfdf68442be56b23137886af70f
Sachin_Mhetre <138901374+SachinMhetre678@users.noreply.github.com>
2024-10-27 15:14:31 +0530

    Deployment changes done 11

--- app/auth/register/step1/page.tsx
+++ app/auth/register/step1/page.tsx
@@ -1,21 +1,14 @@
 // app/auth/register/step1/page.tsx
-"use client"; // Ensure this directive is at the top
+"use client";
 
 import { useRouter } from "next/navigation";
 import Register from "../../../../components/Register";
 
 const RegisterStep1Page = () => {
   const router = useRouter();
-
-  // You can use router here if needed, e.g., for navigation
-  const handleNavigation = () => {
-    router.push('/auth/register/step2'); // Example usage
-  };
-
   return (
     <div>
       <Register />
-      <button onClick={handleNavigation}>Next Step</button> {/* Example button */}
     </div>
   );
 };
